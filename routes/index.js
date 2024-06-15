11529a1a46d504f7320d18dd8d1b40aa66caefbf
Abdeljalil37 <widadpop@gmail.com>
2024-06-15 16:36:58 +0100

    Add lazy loading for user profile images and file uploads

--- routes/index.js
+++ routes/index.js
@@ -18,4 +18,8 @@ router.get('/disconnect', AuthController.getDisconnect);
 
 router.get('/users/me', UsersController.getMe);
 
+router.get('/users/me', UsersController.getMe);
+
+router.post('/files', FilesController.postUpload);
+
 module.exports = router;
