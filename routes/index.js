2f6cb75ac4abbee4440a2182fda2eed158d23c90
Abdeljalil37 <widadpop@gmail.com>
2024-06-20 22:20:55 +0100

    fix index

--- routes/index.js
+++ routes/index.js
@@ -18,8 +18,6 @@ router.get('/disconnect', AuthController.getDisconnect);
 
 router.get('/users/me', UsersController.getMe);
 
-router.get('/users/me', UsersController.getMe);
-
 router.post('/files', FilesController.postUpload);
 
 router.get('/files/:id', FilesController.getShow);
